60be5eb403aedf6cef11af09d16196312300171a
Gleb Sigurko <booordo@mail.ru>
2017-10-24 18:01:06 +0300

    remake puzzle component

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,19 @@
 window.onload = function() {
-	imageSwapElements = document.getElementsByClassName("js-image-swap");
-	accordionElements = document.getElementsByClassName("js-accordion");
+	const imageSwapElements = document.getElementsByClassName("js-image-swap");
+	const accordionElements = document.getElementsByClassName("js-accordion");
+	const puzzleElements = document.getElementsByClassName("js-puzzle-item");
+	const puzzleLineElements = document.getElementsByClassName("js-puzzle-line");
+	const puzzleConnectElements = document.getElementsByClassName("js-puzzle-connect");
 
 	setElementAction('onmouseenter', imageSwapElements, swapImage);
 	setElementAction('onmouseleave', imageSwapElements, swapImage);
 	setElementAction('onclick', accordionElements, toggleAccordion);
+	setElementAction('onmouseenter', puzzleElements, activateSeporatorHandler);
+	setElementAction('onmouseleave', puzzleElements, activateSeporatorHandler);
+	setElementAction('onmouseenter', puzzleLineElements, activatePuzzleByLine);
+	setElementAction('onmouseleave', puzzleLineElements, activatePuzzleByLine);
+	setElementAction('onmouseenter', puzzleConnectElements, activatePuzzleByConnect);
+	setElementAction('onmouseleave', puzzleConnectElements, activatePuzzleByConnect);
 }
 
 function setElementAction(action, el, fn) {
@@ -46,15 +55,15 @@ function setElementAction(action, el, fn) {
 }
 
 function swapImage(e) {
-	elem = e.target;
-	target = elem.getElementsByClassName('js-image-swap-target')[0];
+	let elem = e.target;
+	let target = elem.getElementsByClassName('js-image-swap-target')[0];
 
 	if (!target) {
 		target = elem;
 	}
 
-	oldSrc = target.getAttribute('src');
-	newSrc = elem.getAttribute('data-src');
+	let oldSrc = target.getAttribute('src');
+	let newSrc = elem.getAttribute('data-src');
 
 	if (!oldSrc || !newSrc) return;
 
@@ -63,10 +72,55 @@ function swapImage(e) {
 }
 
 function toggleAccordion(e) {
-	el = e.target;
-	parentNode = el.parentNode;
+	let el = e.target;
+	let parentNode = el.parentNode;
 
 	if (!el.classList.contains('accordion__title')) return;
 
 	parentNode.classList.toggle('accordion__item_actived');
+}
+
+function activateSeporatorHandler (e) {
+	let el = e.target;
+	activateSeporator (el);
+}
+
+function activateSeporator (el) {
+	let parent = el.parentElement;
+	let sibling = el.previousElementSibling;
+
+	if (!sibling) {
+		let seporators = parent.getElementsByClassName('puzzle__separator');
+		sibling = seporators[seporators.length - 1];
+	}
+
+	sibling.classList.toggle('puzzle__separator_actived')
+}
+
+function activatePuzzleByLine (e) {
+	let el = e.target;
+	let parent = el.parentElement;
+	let nextParentSibling = parent.nextElementSibling;
+
+	if (!nextParentSibling || !nextParentSibling.classList.contains('puzzle__item')) {
+		let allParentItemSiblings = parent.parentElement.getElementsByClassName('puzzle__item');
+		nextParentSibling = allParentItemSiblings[0];
+	}
+
+	parent.classList.toggle('puzzle__separator_actived');
+	nextParentSibling.classList.toggle('puzzle__item_actived');
+}
+
+function activatePuzzleByConnect (e) {
+	let el = e.target;
+	let parent = el.parentElement;
+	let prevParentSibling = parent.previousElementSibling;
+
+	if (!prevParentSibling || !prevParentSibling.classList.contains('puzzle__item')) {
+		let allParentItemSiblings = parent.parentElement.getElementsByClassName('puzzle__item');
+		prevParentSibling = allParentItemSiblings[allParentItemSiblings.length - 1];
+	}
+
+	prevParentSibling.classList.toggle('puzzle__item_actived');
+	activateSeporator (prevParentSibling);
 }
