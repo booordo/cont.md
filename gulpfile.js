2cd4e0727233d73df0ca2a85427a80053d7e7875
Gleb Sigurko <booordo@mail.ru>
2018-04-27 11:10:50 +0300

    27.04.2018

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const gulpSass = require('gulp-sass');
 const wait = require('gulp-wait');
 const browserSync = require('browser-sync');
 const nodemon = require('nodemon');
+const cssc = require('gulp-css-condense');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('default', ['browser-sync', 'sass'], () => {
-	gulp.watch ('./styles/**/*.scss', ['sass']);
-	gulp.watch ('./views/**/*.pug').on('change', browserSync.reload);
+	gulp.watch('./styles/**/*.scss', ['sass']);
+	gulp.watch('./views/**/*.pug').on('change', browserSync.reload);
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
@@ -17,20 +19,29 @@ gulp.task('browser-sync', ['nodemon'], function() {
 	});
 });
 
-gulp.task('nodemon', function (cb) {
+gulp.task('nodemon', function(cb) {
 	var started = false;
-	return nodemon({script: './bin/www'}).on('start', () => {
+	return nodemon({
+		script: './bin/www'
+	}).on('start', () => {
 		if (!started) {
 			cb();
-			started = true; 
-		} 
+			started = true;
+		}
 	});
 });
 
 gulp.task('sass', () => {
 	return gulp.src('./styles/style.scss')
-	.pipe(wait(300))
-	.pipe(gulpSass())
-	.pipe(gulp.dest('./public/stylesheets/'))
-	.pipe(browserSync.stream());
+		.pipe(wait(300))
+		.pipe(gulpSass())
+		.pipe(cssc())
+		.pipe(gulp.dest('./public/stylesheets/'))
+		.pipe(browserSync.stream());
 });
+
+gulp.task('image', () =>
+	gulp.src('./public/images/new_cont/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('./public/images/new_cont/min/'))
+);
